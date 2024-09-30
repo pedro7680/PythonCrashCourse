04702d598092899e5d462e846828549a55d398ba
Peter Hoben <ped1704@gmail.com>
2024-09-30 12:13:56 +0100

    few more files createrd

---
+++ famous_quote2.py
@@ -0,0 +1,11 @@
+"""
+2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous
+person’s name using a variable called famous_person. Then compose your message
+and represent it with a new variable called message. Print your message.
+"""
+
+famous_person = "peter hoben"
+quote = '"Its time to gym"'
+# using f strings
+
+print(f"{famous_person.title()}, {quote}")
---
+++ remove_suffix.py
@@ -0,0 +1,11 @@
+"""
+2-8. File Extensions: Python has a removesuffix() method that works exactly
+like removeprefix(). Assign the value 'python_notes.txt' to a variable called
+filename. Then use the removesuffix() method to display the filename without
+the file extension, like some file browsers do.
+
+"""
+
+filename = "python_notes.txt"
+# send in the string of the suffix to be removed ie .txt
+print(filename.removesuffix(".txt"))
